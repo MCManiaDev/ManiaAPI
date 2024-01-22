a9c125f94bb5236b71d6a0898a918d64a3256455
Blockybomb <bluefieldgamez@gmail.com>
2024-01-22 17:54:30 +0100

    Initial commit

---
+++ src/main/java/org/mania/maniaapi/ManiaAPI.java
@@ -0,0 +1,17 @@
+package org.mania.maniaapi;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ManiaAPI extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
